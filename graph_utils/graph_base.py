de3ff188bdde70509b1ebff14da9888d8f9556eb
Baojian Zhou <bzhou6@albany.edu>
2018-06-20 21:38:09 -0400

    dd

--- graph_utils/graph_base.py
+++ graph_utils/graph_base.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__all__ = ['simu_graph']
+__all__ = ['simu_graph', 'node_pre_rec_fm']
 
 import numpy as np
 
@@ -24,3 +24,15 @@ def simu_graph(num_nodes):
     edges = np.asarray(edges, dtype=int)
     weights = np.asarray(weights, dtype=np.float64)
     return edges, weights
+
+
+def node_pre_rec_fm(true_feature, pred_feature):
+    true_feature, pred_feature = set(true_feature), set(pred_feature)
+    pre, rec, fm = 0.0, 0.0, 0.0
+    if len(pred_feature) != 0:
+        pre = len(true_feature & pred_feature) / float(len(pred_feature))
+    if len(true_feature) != 0:
+        rec = len(true_feature & pred_feature) / float(len(true_feature))
+    if (pre + rec) > 0.:
+        fm = (2. * pre * rec) / (pre + rec)
+    return pre, rec, fm
