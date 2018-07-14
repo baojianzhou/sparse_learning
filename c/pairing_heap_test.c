ca99696cf81b11f98a10ed9b61548ad0de9bac2e
Baojian Zhou <bzhou6@albany.edu>
2018-07-14 14:38:34 -0400

    dd

--- c/pairing_heap_test.c
+++ c/pairing_heap_test.c
@@ -7,15 +7,20 @@
 #include "pairing_heap.h"
 
 int main(int argc, char *argv[]) {
+    int i;
     time_t t;
     printf("test pairing heap\n");
     srand((unsigned) time(&t));
-    PairingHeap *ph = ph_make_heap(1000000);
-    printf("is empty: %d", hp_is_empty(ph));
-    printf("heap_size of heap: %d\n", ph->capacity);
-    for (int i = 0; i < 10000; i++) {
-        double rand_val = (rand() % 10000) * 897;
-        int pay_load = rand() % 50;
+    PairingHeap *ph = ph_make_heap(10);
+    if (hp_is_empty(ph)) {
+        printf("hp is empty.\n");
+    } else {
+        printf("hp is not empty.\n");
+    }
+    printf("capacity of the heap: %d\n", ph->capacity);
+    for (i = 0; i < 10; i++) {
+        double rand_val = (rand() % 100) * 897;
+        int pay_load = rand() % 100;
         printf("insert val: %lf, payload: %d\n", rand_val, pay_load);
         ph_insert(ph, rand_val, pay_load);
     }
@@ -25,13 +30,12 @@ int main(int argc, char *argv[]) {
     int *min_payload = (int *) malloc(sizeof(int));
     ph_get_min(ph, min_val, min_payload);
     printf("minimal node val: %lf payload: %d\n", *min_val, *min_payload);
-    ph_add_to_heap(ph, 10.);
+    ph_add_to_heap(ph, 10000.);
     printf("add 10. to heap.\n");
     ph_get_min(ph, min_val, min_payload);
     printf("minimal node val: %lf payload: %d\n", *min_val, *min_payload);
-    for (int i = 0; i < 9000; i++) {
+    for (i = 0; i < 9; i++) {
         ph_delete_min(ph, min_val, min_payload);
-        exit(0);
         printf("delete val: %lf, payload: %d\n", *min_val, *min_payload);
     }
     printf("how many left: %ld\n", ph->hp_size);
